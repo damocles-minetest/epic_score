22bc26d8eb651e2d3fd10819e1b26c663bb0056e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-05 08:33:02 +0100

    highscore stub

---
+++ highscore.lua
@@ -0,0 +1,20 @@
+minetest.register_node("epic_score:highscore", {
+	description = "Epic highscore block",
+	tiles = {
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png",
+		"epic_node_bg.png^epic_dollar_2.png",
+	},
+	paramtype2 = "facedir",
+	groups = {cracky=3,oddly_breakable_by_hand=3,epic=1},
+	on_rotate = screwdriver.rotate_simple,
+
+	on_construct = function(pos)
+    local meta = minetest.get_meta(pos)
+		meta:set_string("topic", "Example quest")
+  end
+
+})
